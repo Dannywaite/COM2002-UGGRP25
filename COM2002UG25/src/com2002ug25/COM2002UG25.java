28202ea4f360153872b30bd1de4ba2c7076fdb13
fea13dw <dwaite1@sheffield.ac.uk>
2015-12-10 17:35:42 +0000

    register working

--- COM2002UG25/src/com2002ug25/COM2002UG25.java
+++ COM2002UG25/src/com2002ug25/COM2002UG25.java
@@ -44,62 +44,31 @@ finally {
 
 
 
-
-
-
-
-
 public static void main(String[] args) throws SQLException {
+    String DB="jdbc:mysql://stusql.dcs.shef.ac.uk/team025?user=team025&password=a2dc8801";
 
-String DB="jdbc:mysql://stusql.dcs.shef.ac.uk/team025?user=team025&password=a2dc8801";
+    Connection con = null; 
+    try { 
+     con = DriverManager.getConnection(DB);
+     System.out.println("connectsuccess");
+    }
+    catch (SQLException ex) {
+     ex.printStackTrace();
+    }
+    /*
+    finally {
+    if (con != null) con.close();
+    System.out.println("closed");
+    }
+    */
+  
+     java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Patientreg().setVisible(true);
+            }
+        });
+    
+    registerPatient("Danny","1995-07-18","07889965789","86","S10 1NU","Maintenance", con);
 
-Connection con = null; 
-try { 
- con = DriverManager.getConnection(DB);
- System.out.println("connectsuccess");
-}
-catch (SQLException ex) {
- ex.printStackTrace();
-}
-
-registerPatient("Danny","1995-07-18","07889965789","86","S10 1NU","Maintenance", con);
-
-
-/*
-finally {
- if (con != null) con.close();
- System.out.println("closing");
-}
-*/
-/*
-String name = "Danny";
-String bday = "1995";
-String phoneno = "hello";
-String housenum = "455";
-String postcode = "123";
-String plan = "twatS";
-System.out.println(name+"', '"+bday+"', '"+phoneno+ "', '"+housenum+"', '"+postcode+"', '"+plan);
-*/     
-/*
-Statement stmt = null; 
-try {
- stmt = (Statement) con.createStatement(); // create from open connection
- int count = stmt.executeUpdate("INSERT INTO patients" + "(name, dob, contact, number, postcode, plan) VALUES ('pls','1995-06-18','07889965789','86','S11 1NU', NULL)");
-
-}
-catch (SQLException ex) {
- ex.printStackTrace();
-}
-finally {
- if (stmt != null) stmt.close();
-}
-*/
-
-//Date date = valueOf("1995-07-18"); // format: valueOf(String) where string is yyyy-mm-dd
-//System.out.println(date.toString());
-//Patient person = new Patient("Danny", "1995-07-18", "7889965789", 60, "s6 3gp", "fuck off");
-
-//System.out.println(person.toString());
- 
 }
 }  
