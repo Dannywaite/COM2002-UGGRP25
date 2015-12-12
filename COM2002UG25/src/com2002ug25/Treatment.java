1a0af39aad891ddae39acd4287560d1fc7c6095f
aca14jg <aca14jg@M64C8CBB8085EBA.shefuniad.shef.ac.uk>
2015-12-12 21:19:29 +0000

    added TreatmentId as a parameter and get/set methods

--- COM2002UG25/src/com2002ug25/Treatment.java
+++ COM2002UG25/src/com2002ug25/Treatment.java
@@ -6,14 +6,50 @@ package com2002ug25;
  */
 public class Treatment {
     
+    int treatmentId;
     String name;
     String type;
     int cost;
     
 //constructor
-    public Treatment (String name, String type, int cost){
+    public Treatment (int treatmentId, String name, String type, int cost){
+        this.treatmentId = treatmentId;
         this.name = name;
         this.type = type;
         this.cost = cost;
     }
+    
+        public int getTreatmentId() {
+        return treatmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setTreatmentId(int treatmentId) {
+        this.treatmentId = treatmentId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+    
+    
 }
